47ce6c3755abfaec54ab76ee41de418e449e2ce9
e-graine <72887022+e-graine@users.noreply.github.com>
2021-03-24 17:00:19 +0100

    Update doc.js

--- doc.js
+++ doc.js
@@ -44,19 +44,71 @@ doc = {
   doc3: [
     {
       type: "title",
-      content: "Ceci est un titre doc3",
+      content: "Passer au zéro déchet, la méthode des 5 R",
     },
     {
       type: "subTitle",
-      content: "Ceci est un sous-titre",
+      content: "1. Refuser",
+    },
+    {
+      type: "paragraph",
+      content:
+        "Les objets dont vous n’avez pas besoin ou ceux à usage unique. Pour cela il faut prévoir des objets réutilisables (gourde, beewrap, sac en tissus..).",
+    },
+    {
+      type: "subTitle",
+      content: "2. Réduire",
+    },
+    {
+      type: "paragraph",
+      content:
+        "Sa consommation en repensant ses besoins. Des produits de bases très économiques permettent répondre à de nombreuses utilités et d’éviter l’achat de produits transformés sur-emballés. ",
+    },{
+      type: "subTitle",
+      content: "3. Réutiliser",
+    },
+    {
+      type: "paragraph",
+      content:
+        "Avant d’être un déchet, un objet peut trouver une nouvelle utilité. Vous pouvez l’utiliser en l’état ou en faire autre chose.",
+    },{
+      type: "subTitle",
+      content: "4. Recycler",
+    },
+    {
+      type: "paragraph",
+      content:
+        "Le tri n’est pas encore optimisé. Pour trier correctement, renseignez-vous auprès de votre collectivité.",
+    },{
+      type: "subTitle",
+      content: "5. Rendre à la terre",
+    },
+    {
+      type: "paragraph",
+      content:
+        "Le compostage permet de valoriser 27% de nos déchets ménagers en engrais. Un compost bien géré reste sain et sans odeurs !",
     },
     {
       type: "title",
-      content: "Ceci est un autre titre",
+      content: "Fabriquer un lombricomposteur",
+    },
+    {
+      type: "paragraph",
+      content:
+        "Cette pratique, idéale lorsque l’on vit en appartement, permet de composter les déchets de cuisine grâce à des vers de terre. Vous pouvez placer le lombricomposteur à l’intérieur de votre logement (dans la cuisine, à la cave, dans le local poubelle..) ou à l’extérieur (sur une terrasse ou un balcon) à l’abris du soleil et de la pluie.",
+    },
+    {
+      type: "paragraph",
+      content:
+        "Il est possible de fabriquer un lombricomposteur avec de la récup ! Il vous suffira de 3 seaux en plastique alimentaire, une perceuse, un cutter et des vers ! La technique est simple : les vers transformeront nos déchets en compost au fûr et à mesure que nous remplirons les seaux.",
+    },{
+      type: "paragraph",
+      content:
+        "Vous trouverez le tutoriel complet sur le site de la Ressourcière, à la suite du jeu.",
     },
     {
       type: "image",
-      content: "img/image.jpg",
+      content: "img/lombricomposteur.png",
     },
   ],
 };
